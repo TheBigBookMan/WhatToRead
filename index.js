0fd4f14cbb0c33f305d4f7af6dcb407bf30d7a90
Ben Smerd <bsmerd@adam.com.au>
2022-11-21 09:20:43 +1030

    comments for idea

---
+++ index.js
@@ -0,0 +1,21 @@
+//* library book system that will have a book database
+
+//* use postgres database and Prisma as the ORM
+
+//* command line interface using nodejs (could try use python but think might take too long to learn)
+
+//* use graphql as API
+
+// * use inquirer for user to search the database based on criteria
+
+//* use randomisers to seed data with X amount in stock
+
+// * use a third party api to get book info
+
+// * have a checkout system as well based on user authentication
+
+// * might be able to use JWT for authorization but will have to check or use something else (explore around what other authorization methods there are)
+
+// * user login to use the application
+
+//* can add a favouriting/review system for users so they can see what other people think of the books they are browsing

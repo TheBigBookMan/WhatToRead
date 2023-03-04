f71e256555d4b158061e4f6b8570c161e87d5ff3
Ben Smerd <bsmerd@adam.com.au>
2023-03-04 11:13:48 +1030

    install pyqt6 and create the basic layout of the gui application

---
+++ gui.py
@@ -0,0 +1,17 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("WhatToRead")
+        self.setMinimumSize(800, 600)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
+    

6bac3317a3927b1a420de977b1f6126b74973ad0
Ben Smerd <bsmerd@adam.com.au>
2023-03-04 10:58:18 +1030

    added new files

--- main.py
+++ main.py
@@ -2,26 +2,14 @@
 # use the pyqt6 stuff for a GUI!?!?!? easy to make with a small database like SQLITE
 #* library book system that will have a book database
 
-#* use SQLLite or MYSQL database and Python as the backend
-
-# * Make it a full stack application for book inquiries with react front end and python backend--- maybe use Django!?!??!
-
-#* use graphql as API MAYBEEE
-
-
+#* use MYSQL database and Python as the backend
 
 #* use randomisers to seed data with X amount in stock
 
 # * use a third party api to get book info
 
-# * have a checkout system as well based on user authentication
-
-# * might be able to use JWT for authorization but will have to check or use something else (explore around what other authorization methods there are)
 
 # * user login to use the application
 
-#* can add a favouriting/review system for users so they can see what other people think of the books they are browsing
-
-#* use ascii logo for the thing and the console.table for how it should show the information
 
 #* browse around  packages for interesting things that may help or make it even better

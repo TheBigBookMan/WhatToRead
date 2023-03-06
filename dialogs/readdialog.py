e1ba8974523fd2631d69ac89d7cd32a83eb8e35e
Ben Smerd <bsmerd@adam.com.au>
2023-03-06 12:17:16 +1030

    add in read books layout

--- dialogs/readdialog.py
+++ dialogs/readdialog.py
@@ -1,6 +1,15 @@
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QGridLayout, QLabel
 
 class ReadDialog(QDialog):
     def __init__(self):
         super().__init__()
         # ? Dialog that will have the Read listed, user can add/remove from
+        self.setWindowTitle("Read")
+        self.setFixedWidth(500)
+        self.setFixedHeight(500)
+
+        layout = QGridLayout()
+        title = QLabel("Read")
+        layout.addWidget(title, 0, 0, 1, 2)
+
+        self.setLayout(layout)

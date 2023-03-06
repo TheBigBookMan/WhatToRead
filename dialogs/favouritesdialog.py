48b77f45b2e809e6a9d242e5dea407c78573d952
Ben Smerd <bsmerd@adam.com.au>
2023-03-06 12:16:22 +1030

    add in favourites dialog layout

--- dialogs/favouritesdialog.py
+++ dialogs/favouritesdialog.py
@@ -1,6 +1,15 @@
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QGridLayout, QLabel
 
 class FavouritesDialog(QDialog):
     def __init__(self):
         super().__init__()
         # ? Dialog that will have the favourites listed, user can add/remove from
+        self.setWindowTitle("Favourites")
+        self.setFixedWidth(500)
+        self.setFixedHeight(500)
+
+        layout = QGridLayout()
+        title = QLabel("Favourites")
+        layout.addWidget(title, 0, 0, 1, 2)
+
+        self.setLayout(layout)

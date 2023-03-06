06d276e44913a6651c62b590e7db91ec0a39e35f
Ben Smerd <bsmerd@adam.com.au>
2023-03-06 12:18:30 +1030

    added in layout for the to read dialog

--- dialogs/toreaddialog.py
+++ dialogs/toreaddialog.py
@@ -1,6 +1,15 @@
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QGridLayout, QLabel
 
 class ToReadDialog(QDialog):
     def __init__(self):
         super().__init__()
         # ? Dialog that will have the to-read listed, user can add/remove from
+        self.setWindowTitle("To Read")
+        self.setFixedWidth(500)
+        self.setFixedHeight(500)
+
+        layout = QGridLayout()
+        title = QLabel("To Read")
+        layout.addWidget(title, 0, 0, 1, 2)
+
+        self.setLayout(layout)
